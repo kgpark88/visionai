dd5dee433ce9ef95c10542de13b72092c232a3fd
Danny Park <kgpark88@gmail.com>
2023-10-23 10:04:08 +0900

    Delete day3 directory

--- day3/flask_web/hello.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__, static_folder='static', template_folder='templates')
-
-@app.route('/hello')
-def hello(name=None):
-    return render_template('hello.html')
-
--- day3/flask_web_te/hello.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__, static_folder='static', template_folder='templates')
-
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-	return render_template('hello.html', name=name)
-
