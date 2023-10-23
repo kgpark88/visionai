3349ecfbfdc694818953067918b22a331a88efb2
Danny Park <kgpark88@gmail.com>
2023-10-24 07:50:30 +0900

    Add files via upload

---
+++ gradient.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+w = tf.Variable(1.0)
+b = tf.Variable(0.5)
+print(w.trainable, b.trainable)
+
+x = tf.convert_to_tensor([1.4])
+y = tf.convert_to_tensor([2.1])
+
+with tf.GradientTape() as tape:
+    z = tf.add(tf.multiply(w, x), b)
+    loss = tf.reduce_sum(tf.square(y - z))
+
+dloss_dw = tape.gradient(loss, w)
+
+tf.print('dL/dw : ', dloss_dw)
+
